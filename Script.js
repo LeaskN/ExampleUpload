ae83108b8a1548730ddee3dfe805ca0197278a02
LeaskN <leask.nicholas@gmail.com>
2020-03-11 18:06:41 -0400

    first commit

---
+++ Script.js
@@ -0,0 +1,29 @@
+// var number = 6
+// var name = ""
+// var bool = false;
+// var thing = null;
+// var steve
+// console.log(number +"; "+typeof number)
+// console.log(name +"; "+typeof name)
+// console.log(bool +"; "+typeof bool)
+// console.log(null +"; "+typeof null)
+// console.log(steve)
+
+
+let Charmander = {
+    name: "Charmander",
+    stage: 1
+}
+let Charmeleon = {
+    name: "Charmeleon",
+    stage: 2
+}
+let Charizard = {
+    name: "Charizard",
+    stage: 3
+}
+let pokemon = [Charmander, Charmeleon, Charizard]
+for(let i=0; i < pokemon.length; i++){
+    console.log(pokemon[0].name)
+
+}
